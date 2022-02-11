8e40ca023227aa869df1ca7937751298457cfe7a
AbinMuds <abinashmudbhari@gmail.com>
2022-02-10 20:47:50 -0700

    adds Route for login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
  import Home from './Home';
  import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
  import Checkout from "./Checkout"
+ import Login from './Login'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <div className="app">
         <Routes>
+          <Route path="/login" element={<Login />} />
           <Route path="/checkout" element={<><Header/><Checkout /></>} />
           <Route path="/" element={<><Header/><Home/></>}/>
         </Routes>
